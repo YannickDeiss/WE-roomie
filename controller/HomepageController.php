320526e0c587585bdee778827740168fe27b3c32
FHNWGery <BaDru1700>
2018-05-07 13:57:43 +0200

    Changes

---
+++ controller/HomepageController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yardie
+ * Date: 05.01.2018
+ * Time: 15:31
+ */
+
+namespace controller;
+
+use service\ListingServiceImpl;
+use view\LayoutRendering;
+use view\TemplateView;
+
+class HomepageController
+{
+    public static function show()
+    {
+        $contentView = new TemplateView("view/dist/landing/landing.php");
+        $contentView->listings = (new ListingServiceImpl())->findTopThree();
+        LayoutRendering::basicLayout($contentView);
+    }
+}
---
+++ view/footer.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="assets/footer/assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/footer/assets/fonts/ionicons.min.css">
+<link rel="stylesheet" href="assets/footer/assets/css/Footer-Basic.css">
+<link rel="stylesheet" href="assets/footer/assets/css/styles.css">
+    <div class="footer-basic">
+        <footer>
+            <div class="social"><a href="https://github.com/Yardie83/roomie"><i class="icon ion-social-github"></i></div>
+            <ul class="list-inline">
+                <li><a href="<?php echo $GLOBALS["ROOT_URL"]; ?>">Home </a></li>
+            </ul>
+            <p class="copyright" style="color:rgb(244,71,107);">With <i class="glyphicon glyphicon-heart"></i> for FHNW; Hermann Grieder, Loris Grether, Tobias Gerhard</p>
+        </footer>
+    </div>
+</body>
+
+</html>
