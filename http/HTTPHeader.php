2b4fa500b737ed28e7a6a1b9c326d838ae3c6fbc
Hermann Grieder <hermann.grieder@students.fhnw.ch>
2018-05-07 14:41:06 +0200

    Created HTTP files

--- http/HTTPHeader.php
+++ http/HTTPHeader.php
@@ -5,3 +5,13 @@
  * Date: 07.05.2018
  * Time: 12:44
  */
+
+namespace http;
+class HTTPHeader implements HTTPStatusCode
+{
+    use HTTPStatusHeader;
+    public static function redirect($redirect_path, $statusCode = HTTPStatusCode::HTTP_301_MOVED_PERMANENTLY) {
+        header("Location: " . $GLOBALS["ROOT_URL"] . $redirect_path, true, self::getStatusCodeNumber($statusCode));
+        exit;
+    }
+}
