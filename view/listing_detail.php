07b720ed876544deadc4a2ad2136878aabc58af0
Yardie83 <hermann.grieder@gmail.com>
2018-05-15 22:34:03 +0200

    added listing_deatil page including the new css styles

--- view/listing_detail.php
+++ view/listing_detail.php
@@ -1,10 +1,59 @@
 <main class="content">
     <div class="listing-detail-grid">
         <div class="image-area">Image Area</div>
-        <div class="details-area">Details Area</div>
-        <div class="contact-area">Contact Area</div>
+        <div class="details-area">
+            <div class="listing-item">
+                <div class="listing-info">
+                    <h2>Nice 3 room apartment in Basel</h2>
+                    <h3>Basel, Basel-Stadt</h3>
+                    <p>Die freundliche, top renovierte 2 Zimmer Wohnung befindet sich an zentraler Lage im 4.OG mit
+                        Lift. Die Wohnung verfügt über ein neues Bad und eine moderne Küche mit Glaskeramik, Geschirrspüler,
+                        Luftabzug, Laminat-Böden, wie auch über ein Kellerabteil. Waschküche und Trockenraum zur
+                        Mitbenützung.
+                    </p>
+                    <div class="listing-details">
+                        <ul>
+                            <li>Rent: 1500 CHF</li>
+                            <li>Size: 150 m2</li>
+                            <li>Rooms: 4</li>
+                            <li>From: 01.06.2018</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="contact-area">
+            <div class="contact-details">
+                <h1>Contact</h1>
+                <p>Hermann Grieder</p>
+                <p>email@email.com</p>
+                <p>079 000 00 00</p>
+            </div>
+            <div class="contact-form">
+                <div class="form-layout contact">
+                    <form class="entry-form contact" method="post" action="">
+                        <h1>Contact the advertiser</h1>
+                        <div class="form-group">
+                            <input type="text" required/>
+                            <label>First Name</label>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" required/>
+                            <label>Last Name</label>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" required/>
+                            <label>E-mail</label>
+                        </div>
+                        <div class="form-group">
+                            <textarea name="description" cols="40" rows="5" required></textarea>
+                            <label class="label-top">Message</label>
+                        </div>
+                        <input type="button" class="green" value="Post" />
+                    </form>
+                </div>
+            </div>
+        </div>
         <div class="map-area">Map Area</div>
     </div>
 </main>
-<footer class="footer">footer</footer>
-</div>
