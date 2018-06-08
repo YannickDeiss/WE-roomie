3c3127955671fa37675ba3994ca1a5e7b6cfc481
FHNWGery <BaDru1700>
2018-06-08 10:58:43 +0200

    PDF

--- view/listing_detail.php
+++ view/listing_detail.php
@@ -40,7 +40,7 @@ $listing = $this->listing[0];
                     </div>
                 </div>
                 <div class="options">
-                    <a href='<?php echo $GLOBALS["ROOT_URL"]; ?>/pdf/{id}<?php echo $listing->getId(); ?>'>
+                    <a href='<?php echo $GLOBALS["ROOT_URL"]; ?>/pdf/<?php echo $listing->getId(); ?>'>
                         <i class="far fa-file-pdf"></i>PDF</a>
                 </div>
             </div>
